3d15b0166672308a1a3ca8915c89695031181d98
Joao-Vitor-2008 <joaovitornascimentolopes4@gmail.com>
2025-10-06 10:29:12 -0300

    Testando o treinamento de randon forest, adicionando atributos a estacao adicionando smile no pom

--- src/main/java/com/exemplo/classes/Estacao.java
+++ src/main/java/com/exemplo/classes/Estacao.java
@@ -2,31 +2,57 @@ package com.exemplo.classes;
 
 public class Estacao {
 
-    private String id;
-    private int temperaturaAr;
-    private int umidadeAr;
+  private String id;
+  private int temperaturaAr;
+  private int umidadeAr;
+  private double presaoAr;
 
-    // Construtor padrão (necessário para Jackson)
-    public Estacao() {}
+  // Construtor padrão (necessário para Jackson)
+  public Estacao() {
+  }
 
-    // Getters e Setters
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
+  // Getters e Setters
 
-    public int getTemperaturaAr() { return temperaturaAr; }
-    public void setTemperaturaAr(int temperaturaAr) { this.temperaturaAr = temperaturaAr; }
+  public double getPressaoAr() {
+    return presaoAr;
+  }
 
-    public int getUmidadeAr() { return umidadeAr; }
-    public void setUmidadeAr(int umidadeAr) { this.umidadeAr = umidadeAr; }
+  public void setPressaoAr(double presaoAr) {
+    this.presaoAr = presaoAr;
+  }
 
-    private static Estacao instance = new Estacao();
-    public static Estacao getInstance() {
-      return instance;
-    }
+  public String getId() {
+    return id;
+  }
 
+  public void setId(String id) {
+    this.id = id;
+  }
 
-    @Override
-    public String toString() {
-        return "Estacao [ID=" + id + ", Temp=" + temperaturaAr + ", Umidade=" + umidadeAr + "]";
-    }
+  public int getTemperaturaAr() {
+    return temperaturaAr;
+  }
+
+  public void setTemperaturaAr(int temperaturaAr) {
+    this.temperaturaAr = temperaturaAr;
+  }
+
+  public int getUmidadeAr() {
+    return umidadeAr;
+  }
+
+  public void setUmidadeAr(int umidadeAr) {
+    this.umidadeAr = umidadeAr;
+  }
+
+  private static Estacao instance = new Estacao();
+
+  public static Estacao getInstance() {
+    return instance;
+  }
+
+  @Override
+  public String toString() {
+    return "Estacao [ID=" + id + ", Temp=" + temperaturaAr + ", Umidade=" + umidadeAr + "]";
+  }
 }
---
+++ src/main/java/com/exemplo/classes/TreinamentoModelo.java
@@ -0,0 +1,11 @@
+package com.exemplo.classes;
+
+import smile.classification.RandomForest;
+import smile.validation.metric.Accuracy;
+
+public class TreinamentoModelo {
+
+  public static void main(String[] args) {
+
+  }
+}
