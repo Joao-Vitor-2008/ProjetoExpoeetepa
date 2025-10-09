f49a9820590c28672fe10ee7e185b7c2ab2ebbcc
Joao-Vitor-2008 <joaovitornascimentolopes4@gmail.com>
2025-10-09 18:41:11 -0300

    Modificando a classe de conexao

--- src/main/java/com/exemplo/ConexaoBanco/ConexaoMysql.java
+++ src/main/java/com/exemplo/ConexaoBanco/ConexaoMysql.java
@@ -6,4 +6,28 @@ import java.sql.SQLException;
 
 public class ConexaoMysql {
 
+  public static String STATUS = "Não conectado";
+
+  private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+  private static final String url = "";
+  private static final String user = "root";
+  private static final String password = "senha";
+
+  public ConexaoMysql() {
+
+  }
+
+  Connection connection = null;
+
+  public static java.sql.Connection getConnection() {
+    try {
+      Class.forName(DRIVER);
+      return DriverManager.getConnection(url, user, password);
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+
+    }
+  }
+
 }
