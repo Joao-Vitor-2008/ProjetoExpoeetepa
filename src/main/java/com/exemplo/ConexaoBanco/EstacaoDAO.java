e5775138f7b8ccd6420457599c32a7454e4026b8
Joao-Vitor-2008 <joaovitornascimentolopes4@gmail.com>
2025-10-23 09:47:40 -0300

    Arrumando as conexões com o bd

--- src/main/java/com/exemplo/ConexaoBanco/EstacaoDAO.java
+++ src/main/java/com/exemplo/ConexaoBanco/EstacaoDAO.java
@@ -8,15 +8,17 @@ import com.exemplo.Estacao.Estacao;
 public class EstacaoDAO {
 
   public void inserirDadosEstacao(Estacao estacao) {
-    String sql = ("INSERT INTO estacoes (id, nome, temperaturaAr, umidadeAr, pressaoAr) VALUES (?,?,?,?);");
+    String sql = ("INSERT INTO estacoes (data_hora, nome, temperaturaAr, umidadeAr, pressaoAr, indice_uv) VALUES (?,?,?,?,?,?);");
 
     try (
         Connection conn = ConexaoMysql.getConnection();
         PreparedStatement stmt = conn.prepareStatement(sql);) {
-      stmt.setString(1, estacao.getNome());
-      stmt.setDouble(2, estacao.getTemperaturaAr());
-      stmt.setDouble(3, estacao.getUmidadeAr());
-      stmt.setInt(4, estacao.getPressaoAr());
+      stmt.setTimestamp(1, estacao.getData_hora());
+      stmt.setString(2, estacao.getNome());
+      stmt.setDouble(3, estacao.getTemperaturaAr());
+      stmt.setDouble(4, estacao.getUmidadeAr());
+      stmt.setInt(5, estacao.getPressaoAr());
+      stmt.setInt(6, estacao.getIndice_uv());
 
       stmt.executeUpdate();
     } catch (SQLException e) {
